b05a9d3972bb3115d7f723e6120cdbff36d9614b
ycs-201607083 <nan0805@naver.com>
2025-01-15 16:14:31 +0900

    fix:코드수정

--- po_pol/vite.config.js
+++ po_pol/vite.config.js
@@ -4,14 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/my_pofo/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        spa: "./404.html", // 추가
-      },
-    },
-  },
   plugins: [react()],
   server: {
     proxy: {
